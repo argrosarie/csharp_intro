4fbebfd62def950e47395021c89c06f0a90cca5c
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-03 17:14:19 -0300

    first app: sum

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -6,7 +6,17 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ya wey");
+            int sideA;
+            int sideB;
+            int result;
+            Console.WriteLine("Calcula area de rectangulo");
+            Console.WriteLine("Ingrese valor de lado A");
+            sideA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese valor de lado B");
+            sideB = Convert.ToInt32(Console.ReadLine());
+
+            result = sideA * sideB;
+            Console.WriteLine("El resultado es: " + result);
         }
     }
 }
