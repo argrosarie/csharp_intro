1ec6eb14ff9d3b8c292c68c3c13fcf0427d9bb7f
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-05 23:53:06 -0300

    Constants

---
+++ constantes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ratio = 0d;
+            var result = 0d;
+
+            Console.WriteLine("Welcome, let's calculate some circles!");
+
+            //PI * ratio * ratio
+            Console.WriteLine("Please enter ratio's value");
+            ratio = Convert.ToDouble(Console.ReadLine());
+
+            result = Math.PI * ratio * ratio;
+
+            Console.WriteLine("The result is : " + result);
+        }
+    }
+}
