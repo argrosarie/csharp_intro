cf0765dcfb19b264e1f2c7bcbf5d8e1fa82026bb
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-08 20:54:46 -0300

    Logic operators and and or

---
+++ logicOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace logicOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value1 = true;
+            bool value2 = true;
+            bool value3 = true;
+            bool result = value1 && value2;
+
+            Console.WriteLine(result);
+
+            bool resultOr = value1 || value2 || value3;
+            Console.WriteLine(resultOr);
+
+        }
+    }
+}
