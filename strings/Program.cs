d7229587a4c8f44e10869442c4f2df7af252e77b
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-10 18:54:42 -0300

    Strings

---
+++ strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pricess Leia ID");
+            string firstName = "Leia";
+            string lastName = "Organa";
+            int age = 27;
+            var profession = "Warrior";
+
+            string card = $"Princess {firstName}\n" +
+            $"Name: {firstName} {lastName}\n" +
+            $"Age: {age}\n" +
+            $"Profession: {profession}\n";
+
+            Console.WriteLine(card);
+        }
+    }
+}
